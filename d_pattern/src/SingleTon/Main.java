1362a21a93fb5a5396eeaef44d468da2c1d8c03a
leegibeom <zabioz00@gmail.com>
2019-06-25 11:36:15 +0900

    修正しました。２

--- d_pattern/src/SingleTon/Main.java
+++ d_pattern/src/SingleTon/Main.java
@@ -3,34 +3,19 @@ package SingleTon;
 public class Main {
 
 	public static void main(String[] args) {
-
-	Satan satan1 = Satan.getInstance();
-	Satan satan2 = Satan.getInstance();
-
-	Satan hp1 = Satan.getInstance();
-	Satan hp2 = Satan.getInstance();
-
-	System.out.println(hp1);
-	System.out.println(hp2);
+		
+	Satan satan1 = Satan.getInstance(); //satan1이라는 인스턴스를 호출함
+	Satan satan2 = Satan.getInstance(); //2도 호출
 	
+	satan1.setHp(50); //satan1 인스턴스에 hp를 입력
 	
-	System.out.println(satan1.getHp());
-	System.out.println(satan2.getHp());
+	System.out.println(satan1.getHp()); //1,2를 출력함
+	System.out.println(satan2.getHp()); 
 	
-	hp1.setHp(90);
-	System.out.println(satan1.getHp());
-	System.out.println(satan2.getHp());
+	satan2.setHp(33); //이번엔 satan2 인스턴스에 hp를 입력
 	
-	hp2.setHp(60);
-	System.out.println(satan1.getHp());
-	System.out.println(satan2.getHp());
+	System.out.println(satan1.getHp());	//1,2를 출력함
+	System.out.println(satan2.getHp()); 	
 	
-	
-	if (hp1 == hp2) {
-		System.out.println("satan1과 satan2는 같습니다");
-	}
-	else {
-		System.out.println("satan1과 satan2는 다릅니다");
-		}
 	}
 }
--- d_pattern/src/SingleTon/Satan.java
+++ d_pattern/src/SingleTon/Satan.java
@@ -1,24 +1,25 @@
 package SingleTon;
 
 public class Satan {
-	private static Satan satan = new Satan();
-	private int hp;
+	private static Satan satan;
+	private int hp; //변수선언
 	
 	private Satan() {
-		hp = 100;		
+		hp = 100; //값대입
 	}
 
-	public static Satan getInstance() {
+	public static Satan getInstance() { //메소드 추가
 		if(satan == null) {
-			satan = new Satan();
+			satan = new Satan(); 
 		}
-		return satan;
+		return satan;  //사탄의 값이 없을때 새로운 사탄을 만들어내고 반환한다
+		
 	}
 	
-	public int getHp() {
+	public int getHp() { //입력값이 없는 메소드 추가
 		return hp;
 	}
-	public void setHp(int hp) {
+	public void setHp(int hp) { //리턴값이 없는 메소드 추가 this는 자신의 hp를 지정
 		this.hp = hp;
 	}
 	
