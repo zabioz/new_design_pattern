d7448bcb8d1beadcf68af5181de0b182301d0862
leegibeom <zabioz00@gmail.com>
2019-06-26 23:54:51 +0900

    修正しました。

--- d_pattern/src/Factory/PowerKnight.java
+++ d_pattern/src/Factory/PowerKnight.java
@@ -2,7 +2,7 @@ package Factory;
 
 public class PowerKnight extends Knight{ //나이트에서 상속받은 클래스생성
 	@Override //상속받은것을 재정의
-	public String getName() { //메소드생성
+	public String getName() { //메소드 정의
 		return "powerknight"; //반환
 	}
 
